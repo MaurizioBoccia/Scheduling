14b37fa48df052c59c23a04c3b1d1673bd32dbd0
AdrianoMasone <adriano.masone@unina.it>
2021-12-10 13:15:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import random
 # Legge l'istanza da un file txt
 #======================================================
 
-# FileName = "Ins_V5_J50_T20_R60_B10_W4_S131_N1.txt"
-FileName = "Ins_V5_J200_T20_R60_B10_W2_S314_N4.txt"
+FileName = "Ins_V5_J50_T20_R60_B10_W4_S131_N1.txt"
+# FileName = "Ins_V5_J200_T20_R60_B10_W2_S314_N4.txt"
 # FileName = "Ins_V2_J10_T10_R60_B6_W1_S0_N0.txt"
 #FileName = input()
 #FileName = sys.argv[1]
